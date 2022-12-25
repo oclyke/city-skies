5fd8a4f90769f3d4d30a473d4ef7f54754afbc9b
Owen <o@oclyke.dev>
2022-12-25 03:20:27 -0700

    reformat manifest.py

--- src/manifest.py
+++ src/manifest.py
@@ -3,9 +3,12 @@
 # freeze("packages/pystorage/src")
 
 # microdot web server
-freeze("packages/microdot/src", [
-  "microdot.py",
-  "microdot_websocket.py",
-  "microdot_asyncio.py",
-  "microdot_asyncio_websocket.py",
-])
+freeze(
+    "packages/microdot/src",
+    [
+        "microdot.py",
+        "microdot_websocket.py",
+        "microdot_asyncio.py",
+        "microdot_asyncio_websocket.py",
+    ],
+)
