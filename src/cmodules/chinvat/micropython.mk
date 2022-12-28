d32d56a58cee9c0daa59c4e5fb68873524831154
Owen <o@oclyke.dev>
2022-12-28 10:37:32 -0700

    Update micropython.mk

--- src/cmodules/chinvat/micropython.mk
+++ src/cmodules/chinvat/micropython.mk
@@ -2,6 +2,7 @@ MOD_DIR := $(USERMOD_DIR)
 
 # Add all C files to SRC_USERMOD.
 SRC_USERMOD += $(MOD_DIR)/src/module.c
+
 SRC_USERMOD += $(MOD_DIR)/src/system/uuidv4.c
 SRC_USERMOD += $(MOD_DIR)/src/system/version.c
 SRC_USERMOD += $(MOD_DIR)/src/system/hardware.c
@@ -9,6 +10,13 @@ SRC_USERMOD += $(MOD_DIR)/src/system/firmware.c
 SRC_USERMOD += $(MOD_DIR)/src/system/network.c
 SRC_USERMOD += $(MOD_DIR)/src/system/identity.c
 SRC_USERMOD += $(MOD_DIR)/src/system/system.c
+
+SRC_USERMOD += $(MOD_DIR)/src/shard/atom.c
+SRC_USERMOD += $(MOD_DIR)/src/shard/variable.c
+SRC_USERMOD += $(MOD_DIR)/src/shard/variable/boolean.c
+SRC_USERMOD += $(MOD_DIR)/src/shard/variable/double.c
+SRC_USERMOD += $(MOD_DIR)/src/shard/variable/integer.c
+SRC_USERMOD += $(MOD_DIR)/src/shard/variable/option.c
 
 SRC_USERMOD += $(MOD_DIR)/third-party/chinvat/third-party/nanopb/pb_common.c
 SRC_USERMOD += $(MOD_DIR)/third-party/chinvat/third-party/nanopb/pb_encode.c
