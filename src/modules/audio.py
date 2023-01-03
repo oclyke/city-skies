585fd4a254f8feaa92dbdc8aa023d534532d167c
Owen <o@oclyke.dev>
2023-01-03 10:33:47 -0700

    Update audio.py

--- src/modules/audio.py
+++ src/modules/audio.py
@@ -19,10 +19,14 @@ class Fft:
 
 
 class AudioSource:
-    def __init__(self, configuration):
+    def __init__(self, name, configuration):
         sample_rate, sample_length = configuration
         self._sample_rate = sample_rate
         self._sample_length = sample_length
+        self._name = name
+
+        # allow for sources to keep variables
+        self._variables = {}
 
         # attempt to pre-allocate all necessary memory
         self._samples = [0.0] * sample_length
@@ -34,3 +38,25 @@ class AudioSource:
     @property
     def fft(self):
         return self._fft
+    
+    @property
+    def variables(self):
+        return self._variables
+    
+    def register_variable(self, variable):
+        self._variables[variable.name] = variable
+
+class AudioManager:
+    def __init__(self):
+        self._sources = []
+        self._selected = None
+
+    @property
+    def sources(self):
+        return self._sources
+    
+    def add_source(self, source):
+        self._sources.append(source)
+
+# create a singleton instance of the audio manager
+audio = AudioManager()
