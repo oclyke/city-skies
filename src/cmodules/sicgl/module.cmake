886b31fb748b2c7080d5ef6438869232a64a909d
Owen <o@oclyke.dev>
2023-01-04 00:01:01 -0700

    fix esp32

--- src/cmodules/sicgl/module.cmake
+++ src/cmodules/sicgl/module.cmake
@@ -8,6 +8,7 @@ target_sources(sicgl INTERFACE
   ${CMAKE_CURRENT_LIST_DIR}/src/field.c
   ${CMAKE_CURRENT_LIST_DIR}/src/interface.c
   ${CMAKE_CURRENT_LIST_DIR}/src/color_sequence.c
+  ${CMAKE_CURRENT_LIST_DIR}/src/drawing/compose.c
   ${CMAKE_CURRENT_LIST_DIR}/src/drawing/global.c
   ${CMAKE_CURRENT_LIST_DIR}/src/drawing/screen.c
   ${CMAKE_CURRENT_LIST_DIR}/src/drawing/interface.c
@@ -17,7 +18,9 @@ target_sources(sicgl INTERFACE
   # sicgl library sources
   ${CMAKE_CURRENT_LIST_DIR}/third-party/sicgl/src/blit.c
   ${CMAKE_CURRENT_LIST_DIR}/third-party/sicgl/src/color_sequence.c
+  ${CMAKE_CURRENT_LIST_DIR}/third-party/sicgl/src/compose.c
   ${CMAKE_CURRENT_LIST_DIR}/third-party/sicgl/src/field.c
+  ${CMAKE_CURRENT_LIST_DIR}/third-party/sicgl/src/gamma.c
   ${CMAKE_CURRENT_LIST_DIR}/third-party/sicgl/src/iter.c
   ${CMAKE_CURRENT_LIST_DIR}/third-party/sicgl/src/screen.c
   ${CMAKE_CURRENT_LIST_DIR}/third-party/sicgl/src/translate.c
