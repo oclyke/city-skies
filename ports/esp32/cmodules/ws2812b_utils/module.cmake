f86656dbe5dcd56520e456374e5991e84ec5001c
Owen <o@oclyke.dev>
2023-01-09 23:59:22 -0700

    fix cmake error about nonexistent include path

--- ports/esp32/cmodules/ws2812b_utils/module.cmake
+++ ports/esp32/cmodules/ws2812b_utils/module.cmake
@@ -5,7 +5,7 @@ target_sources(ws2812b_utils INTERFACE
 )
 
 target_include_directories(ws2812b_utils INTERFACE
-  ${CMAKE_CURRENT_LIST_DIR}/include
+  # ${CMAKE_CURRENT_LIST_DIR}/include
 )
 
 target_link_libraries(usermod INTERFACE ws2812b_utils)
