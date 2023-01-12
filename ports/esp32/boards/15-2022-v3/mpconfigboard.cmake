ff2466b9433161567485025f06a51578a8ec97c2
Owen <o@oclyke.dev>
2023-01-12 11:45:05 -0700

    update 2022-v3 boards to 16MB flash definitions

--- ports/esp32/boards/15-2022-v3/mpconfigboard.cmake
+++ ports/esp32/boards/15-2022-v3/mpconfigboard.cmake
@@ -5,7 +5,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/../common.cmake)
 set(SDKCONFIG_DEFAULTS
     ${MICROPY_PORT_DIR}/boards/sdkconfig.base
     ${MICROPY_PORT_DIR}/boards/sdkconfig.ble
-    boards/sdkconfig.partitions.4MiB
+    boards/sdkconfig.partitions.16MiB
 )
 if(NOT MICROPY_FROZEN_MANIFEST)
     set(MICROPY_FROZEN_MANIFEST ${CMAKE_CURRENT_LIST_DIR}/manifest.py)
