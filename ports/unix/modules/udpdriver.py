98207b91b8db6b59502df36f0b34cd320bac820a
oclyke <o@oclyke.dev>
2023-01-29 15:58:12 -0800

    remove unused screen definition

--- ports/unix/modules/udpdriver.py
+++ ports/unix/modules/udpdriver.py
@@ -1,19 +1,11 @@
 class UDPDriver:
-    def __init__(self, screen, host="0.0.0.0", ports=(6969, 6420)):
+    def __init__(self, host="0.0.0.0", ports=(6969, 6420)):
         import socket
-
-        self._screen = screen
-        self._config = self._screen.width.to_bytes(
-            2, "little"
-        ) + self._screen.height.to_bytes(2, "little")
 
         self._host = host
         self._port_control, self._port_data = ports
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        self._addr_control = socket.getaddrinfo(self._host, self._port_control)[0][-1]
-        self._addr_data = socket.getaddrinfo(self._host, self._port_data)[0][-1]
+        self._addr = socket.getaddrinfo(self._host, self._port_data)[0][-1]
 
     def push(self, interface):
-        # self._sock.sendto(self._config, self._addr_control)
-        self._sock.sendto(interface.memory, self._addr_data)
+        self._sock.sendto(interface.memory, self._addr)
--- ports/unix/variants/dev/modules/hardware.py
+++ ports/unix/variants/dev/modules/hardware.py
@@ -7,10 +7,7 @@ import pysicgl
 hw_version = SemanticVersion.from_semver("0.0.0-unix-dev")
 
 # diplay hardware
-WIDTH = 23
-HEIGHT = 13
-display = pysicgl.Screen((WIDTH, HEIGHT))
 drivers = [
-    UDPDriver(display, "0.0.0.0", (6969, 6420)),
+    UDPDriver("0.0.0.0", (6969, 6420)),
     ArtnetDriver("192.168.4.177"),
 ]
