3b4f0a5592a9308ad1d93258f7728494e1095232
Owen <o@oclyke.dev>
2023-03-08 12:20:07 -0800

    Update utilities.h

--- src/cmodules/ftt/include/utilities.h
+++ src/cmodules/ftt/include/utilities.h
@@ -1,9 +1,10 @@
 #pragma once
 
+#include "fft.h"
 #include "py/obj.h"
 #include "sicgl/extent.h"
-#include "fft.h"
 
 int extract_float_obj(mp_obj_t obj, mp_float_t* value);
 int unpack_float_tuple2(mp_obj_t obj, mp_float_t* u, mp_float_t* v);
-int interpolate_real_outputs_linear(fft_config_t* config, double phase, double* output);
+int interpolate_real_outputs_linear(
+    fft_config_t* config, double phase, double* output);
