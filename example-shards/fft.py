7a3636adc7125b5b6b5791d1f03354fa880938f6
Owen <o@oclyke.dev>
2023-03-08 20:24:56 -0800

    Update fft.py

--- example-shards/fft.py
+++ example-shards/fft.py
@@ -17,3 +17,8 @@ def frames(layer):
         # audio_source.fft.plan.align(strengths)
         reshape_config = (1.0, 0)  # (factor, floor)
         audio_source.fft.plan.reshape(strengths, reshape_config)
+
+        sum, max, max_idx = audio_source.fft.plan.stats()
+        bin_width = audio_source.fft.plan.bin_width
+        strongest_freq = max_idx * bin_width
+        print(f"strongest[{max_idx}]: {strongest_freq:012.2f} hz ({max:012.2f})")
