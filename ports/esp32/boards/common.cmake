a1f2355bc1b30a5d521786cce7454e6886546190
Owen <o@oclyke.dev>
2023-03-13 12:48:21 -0700

    Update common.cmake

--- ports/esp32/boards/common.cmake
+++ ports/esp32/boards/common.cmake
@@ -3,6 +3,8 @@ set(USER_C_MODULES ${USER_C_MODULES}
   ${CMAKE_CURRENT_LIST_DIR}/../../../src/cmodules/sicgl/module.cmake
   ${CMAKE_CURRENT_LIST_DIR}/../../../src/cmodules/noise/module.cmake
   ${CMAKE_CURRENT_LIST_DIR}/../../../src/cmodules/ftt/module.cmake
+  ${CMAKE_CURRENT_LIST_DIR}/../../../src/cmodules/buffer/module.cmake
+  ${CMAKE_CURRENT_LIST_DIR}/../../../src/cmodules/reshape/module.cmake
 
   # esp32 c modules
   ${CMAKE_CURRENT_LIST_DIR}/../cmodules/ws2812b_utils/module.cmake
