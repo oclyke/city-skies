d027aa15b307e4f10d4e14a8c2d61213a90a6d8c
Owen <o@oclyke.dev>
2023-04-04 18:12:40 -0700

    reorganize tools

--- ports/unix/build.sh
+++ ports/unix/build.sh
@@ -1,23 +1,9 @@
-# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
-# (last component of path used to find the script must not be a symlink - there is a multi-line way to do that if needed see above)
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-ROOT_DIR=$SCRIPT_DIR/../..
-
-# the first argument is passed along directly to make as the target
-TARGET=$1
-
-# roots
-MPY_ROOT=$ROOT_DIR/third-party/micropython
-MPY_UNIX_PORT_ROOT=$MPY_ROOT/ports/unix
-
-# default values
-export VARIANT=dev
-export VARIANT_DIR=$SCRIPT_DIR/variants/$VARIANT
-export BUILD=$SCRIPT_DIR/dist/$VARIANT
+# get common locations
+source ./common.sh
 
 # allow easy loading of environment
 ENV_FILE=$SCRIPT_DIR/.env.sh
 [ -f "$ENV_FILE" ] && echo "sourcing $ENV_FILE" && source $ENV_FILE
 
 make -C $MPY_UNIX_PORT_ROOT submodules
-make -C $MPY_UNIX_PORT_ROOT $TARGET
+make -C $MPY_UNIX_PORT_ROOT $1
--- ports/unix/clean-frozen-python.sh
+++ ports/unix/clean-frozen-python.sh
@@ -1,11 +1,9 @@
-# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
-# (last component of path used to find the script must not be a symlink - there is a multi-line way to do that if needed see above)
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# get common locations
+source ./common.sh
 
-# default values
-export VARIANT=dev
-export VARIANT_DIR=$SCRIPT_DIR/variants/$VARIANT
-export BUILD=$SCRIPT_DIR/dist/$VARIANT
+BUILD=$MPY_UNIX_PORT_ROOT/build-$VARIANT
+
+echo "cleaning frozen python files from build at:\n$BUILD"
 
 rm -rf $BUILD/frozen_mpy
 rm -rf $BUILD/genhdr
