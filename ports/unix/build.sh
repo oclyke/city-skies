777e15a08909b44f413e624d02f11f5145539633
Owen <o@oclyke.dev>
2023-04-05 12:16:16 -0700

    Update build.sh

--- ports/unix/build.sh
+++ ports/unix/build.sh
@@ -1,5 +1,9 @@
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+# (last component of path used to find the script must not be a symlink - there is a multi-line way to do that if needed see above)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # get common locations
-source ./common.sh
+source $SCRIPT_DIR/common.sh
 
 # allow easy loading of environment
 ENV_FILE=$SCRIPT_DIR/.env.sh
